691c5ecb0a6d3c7b05e3268debb60f6b6333fcf0
Nick Menshikov <nikmenshikov1@gmail.com>
2026-04-17 15:06:20 +0300

    feat: initialize `flux-search` module with Spring Boot setup and Maven configuration
    - Set up the `flux-search` module with a basic Spring Boot application structure.
    - Added Maven wrapper and configuration for the new module.
    - Introduced essential dependencies (`spring-boot-starter-kafka`, `spring-boot-starter-webmvc`) and plugins.
    - Configured `.gitignore` and `.gitattributes` for the module.
    - Created initial test and application files.

---
+++ flux-search/src/main/java/com/nickmenshikov/flux/search/FluxSearchApplication.java
@@ -0,0 +1,13 @@
+package com.nickmenshikov.flux.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxSearchApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(FluxSearchApplication.class, args);
+    }
+
+}
---
+++ flux-search/src/test/java/com/nickmenshikov/flux/search/FluxSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickmenshikov.flux.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
